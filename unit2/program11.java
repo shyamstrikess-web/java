ea7f5d53c33d50f3268e9fa66ec58dea5d4662c9
shyamstrikess-web <shyamstrikess@gmail.com>
2026-07-24 13:03:05 +0530

    Add files via upload

---
+++ unit2/program11.java
@@ -0,0 +1,30 @@
+//  Write a java program to use Interface in java
+
+interface Animal {
+    void sound();   // Abstract method
+}
+
+// Class implementing the interface
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Another class implementing the interface
+class Cat implements Animal {
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+// Main class
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+
+        a1.sound();
+        a2.sound();
+    }
+}
