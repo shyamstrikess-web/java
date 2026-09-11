d181fdcf6ed550b06a29ec3c47bbc39fd5ccbc4a
shyamstrikess-web <shyamstrikess@gmail.com>
2026-09-11 12:42:38 +0530

    Update program22.java

--- unit3/program22.java
+++ unit3/program22.java
@@ -1,8 +1,9 @@
+//Write a java program to implement simple exception handling
 class Main {
     public static void main(String[] args) {
         try {
             int numbers[] = {1, 2, 3};
-            System.out.println(numbers[5]); // Trying to access an element that doesn't exist
+            System.out.println(numbers[5]); 
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Error: Array index out of bounds!");
         }
