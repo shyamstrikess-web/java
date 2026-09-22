2f51c8661f6567d744c142d9aae985107e8d9758
shyamstrikess-web <shyamstrikess@gmail.com>
2026-09-22 14:31:47 +0530

    Refactor SimpleException class for clarity

--- unit3/program23.java
+++ unit3/program23.java
@@ -2,7 +2,7 @@
 class SimpleException {
     public static void main(String[] args) {
         try {
-            int result = 10 / 0; // Throws ArithmeticException
+            int result = 10 / 0; 
             System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero!");
