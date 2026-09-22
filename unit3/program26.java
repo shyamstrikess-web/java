91dbb6cb7541d61dc9ad3c9ecff360d97d6a6af5
shyamstrikess-web <shyamstrikess@gmail.com>
2026-09-22 14:33:13 +0530

    Refactor ThrowExample class for clarity

--- unit3/program26.java
+++ unit3/program26.java
@@ -2,7 +2,7 @@
 class ThrowExample {
     static void checkAge(int age) {
         if (age < 18) {
-            // Throwing an explicit exception manually
+           
             throw new ArithmeticException("Access denied - You must be at least 18 years old.");
         } else {
             System.out.println("Access granted - You are old enough!");
@@ -11,7 +11,7 @@ class ThrowExample {
 
     public static void main(String[] args) {
         try {
-            checkAge(15); // Will trigger the throw statement
+            checkAge(15); 
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
         }
