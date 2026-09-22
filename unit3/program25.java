c56d255b2832917e13507f526e1ec07d303bdaac
shyamstrikess-web <shyamstrikess@gmail.com>
2026-09-22 14:32:45 +0530

    Format code for SimpleMultipleCatch class

--- unit3/program25.java
+++ unit3/program25.java
@@ -2,7 +2,7 @@
 class SimpleMultipleCatch {
     public static void main(String[] args) {
         try {
-            int a = 10 / 0; // Throws ArithmeticException
+            int a = 10 / 0; 
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero!");
         } catch (ArrayIndexOutOfBoundsException e) {
